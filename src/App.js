5810cb9d41a76d6121d0ec49a2e18ca49578085d
deshanw <deshanshehantha@gmail.com>
2020-10-01 22:42:00 +0530

    test ui need to add more changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,122 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import ProjectManager from "./components/project-manager.component";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card, CardGroup} from "react-bootstrap";
+import img from "./img-business-conference.jpg"
 
-function App() {
-  return (
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 
-      <ProjectManager/>
-  );
+
+export default function App() {
+
+    const classes = useStyles();
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleChange = (event) => {
+        setAuth(event.target.checked);
+    };
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Project Manager
+                    </Typography>
+                    {auth && (
+                        <div>
+                            <IconButton
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <AccountCircle/>
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={open}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                            </Menu>
+                        </div>
+                    )}
+                </Toolbar>
+            </AppBar>
+            <br/><br/>
+            <CardGroup>
+                <Card className="bg-dark text-white">
+                    <Card.Img src={img} alt="Card image" style = {{height : "100%"}}/>
+                    <Card.ImgOverlay style ={{backgroundColor: "rgba(#000, 0.4)"}}>
+                        <Card.Title>Project Manager Tool</Card.Title>
+                        <Card.Text>
+                            Create your project, add your team menmbers and manage your project with us!
+                            Project manager tool will make your life easy !!
+                        </Card.Text>
+                        <Card.Text>Last updated 3 mins ago</Card.Text>
+                    </Card.ImgOverlay>
+                </Card>
+                <Card>
+                    <ProjectManager/>
+                </Card>
+            </CardGroup>
+        </div>
+    );
+
 }
-
-export default App;
